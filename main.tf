33151d99ac1d65a4ae48ada296d42b37a40b1441
phlpMagalhaes <phlpmagalhaes@gmail.com>
2024-05-04 19:18:04 -0300

    Novo provisionamento

--- main.tf
+++ main.tf
@@ -4,29 +4,93 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "my_rg" {
+  name     = "my-resource-group"
   location = "East US"
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                  = "example-vm"
-  resource_group_name   = azurerm_resource_group.example.name
-  location              = azurerm_resource_group.example.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-  admin_password        = "Password1234!"
-  network_interface_ids = [azurerm_network_interface.example.id]
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "my-subnet"
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.my_rg.name  
+}
+
+
+resource "azurerm_public_ip" "my_public_ip" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "my_nsg" {
+  name                = "my-nsg"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowWinRM"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "my_nic" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "my-nic-config"
+    subnet_id                     = azurerm_subnet.my_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_public_ip.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "my_vm" {
+  name                  = "my-vm"
+  location              = azurerm_resource_group.my_rg.location
+  resource_group_name   = azurerm_resource_group.my_rg.name
+  network_interface_ids = [azurerm_network_interface.my_nic.id]
+  size                  = "Standard_B2s"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssw0rd1234"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
   }
 }
 
